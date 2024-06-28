0961f1a0626167b9f56603e724b72e2efb9414c2
shay660-1 <shay.itzhaki@mail.huji.ac.il>
2024-06-28 12:15:17 +0300

    cmake-build

---
+++ cmake-build-debug/CMakeFiles/ex3.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/studies/year_c/semester_B/OS/ex3/Barrier/Barrier.cpp" "/mnt/c/studies/year_c/semester_B/OS/ex3/cmake-build-debug/CMakeFiles/ex3.dir/Barrier/Barrier.cpp.o"
+  "/mnt/c/studies/year_c/semester_B/OS/ex3/Barrier/barrierdemo.cpp" "/mnt/c/studies/year_c/semester_B/OS/ex3/cmake-build-debug/CMakeFiles/ex3.dir/Barrier/barrierdemo.cpp.o"
+  "/mnt/c/studies/year_c/semester_B/OS/ex3/Sample Client/SampleClient.cpp" "/mnt/c/studies/year_c/semester_B/OS/ex3/cmake-build-debug/CMakeFiles/ex3.dir/Sample_Client/SampleClient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../Barrier"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ex3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3.dir/Barrier/Barrier.cpp.o"
+  "CMakeFiles/ex3.dir/Barrier/barrierdemo.cpp.o"
+  "CMakeFiles/ex3.dir/Sample_Client/SampleClient.cpp.o"
+  "ex3"
+  "ex3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
